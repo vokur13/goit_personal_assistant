75eadd79f42f4ad16b3697ffdee54ab3b958aba2
Oleksandr Martyniuk <132834938+CadejoBlanko@users.noreply.github.com>
2023-11-16 21:03:16 +0200

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=Fals)
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = ["*"]
 
